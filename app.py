c412aef012fad2b4c41aa316718c0857f9f54466
Grashal Vishwa <vgrashal@gmail.com>
2025-02-04 14:14:17 +0530

    added more UI and functions for selected user

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import preprocessor
+import preprocessor, helper
 
 st.sidebar.title("WhatsApp Chat Analyser")
 
@@ -11,3 +11,18 @@ if upload_file is not None:
     df = preprocessor.preprocess(data)
 
     st.dataframe(df)
+    
+    #fetching unique users
+    user_list = df['user'].unique().tolist()
+    user_list.remove('chat/group notification')
+    user_list.sort()
+    user_list.insert(0, "Overall")
+    selected_user = st.sidebar.selectbox("Select User: ", user_list)
+
+    if st.sidebar.button("Show Analysis"):
+        num_messages = helper.fetch_stats(selected_user, df)
+        col1, col2, col3, col4 = st.beta_columns(4)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
---
+++ helper.py
@@ -0,0 +1,3 @@
+def fetch_stats(selected_user, df):
+    if selected_user == "Overall":
+        return df.shape[0]
