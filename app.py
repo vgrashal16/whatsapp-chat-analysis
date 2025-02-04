7e7ed042f28a14b1eb07a8b435b56fcedcead6d2
Grashal Vishwa <vgrashal@gmail.com>
2025-02-04 14:45:30 +0530

    number of words and messages + restructuring code

--- app.py
+++ app.py
@@ -13,16 +13,19 @@ if upload_file is not None:
     st.dataframe(df)
     
     #fetching unique users
-    user_list = df['user'].unique().tolist()
+    user_list = df['users'].unique().tolist()
     user_list.remove('chat/group notification')
     user_list.sort()
     user_list.insert(0, "Overall")
     selected_user = st.sidebar.selectbox("Select User: ", user_list)
 
     if st.sidebar.button("Show Analysis"):
-        num_messages = helper.fetch_stats(selected_user, df)
-        col1, col2, col3, col4 = st.beta_columns(4)
+        num_messages,words = helper.fetch_stats(selected_user, df)
+        col1, col2, col3, col4 = st.columns(4)
 
         with col1:
             st.header("Total Messages")
             st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
--- helper.py
+++ helper.py
@@ -1,3 +1,10 @@
 def fetch_stats(selected_user, df):
-    if selected_user == "Overall":
-        return df.shape[0]
+    if selected_user != "Overall":
+        df = df[df['users'] == selected_user]
+
+    num_messages = df.shape[0] #fetching number of messages
+    words=[] #fetching number of words
+    for messages in df['user_messages']:
+        words.extend(messages.split())  
+    return num_messages,len(words)
+ 
