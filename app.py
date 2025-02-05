36f3c2e1c82790448e22d0ba7cad5d5724491308
Grashal Vishwa <vgrashal@gmail.com>
2025-02-05 13:16:02 +0530

    most used words and wordcloud

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import preprocessor, helper
+import matplotlib.pyplot as plt
 
 st.sidebar.title("WhatsApp Chat Analyser")
 
@@ -20,9 +21,9 @@ if upload_file is not None:
     selected_user = st.sidebar.selectbox("Select User: ", user_list)
 
     if st.sidebar.button("Show Analysis"):
+        st.title("Messages Analysis")
         num_messages,words,media_msg,links = helper.fetch_stats(selected_user, df)
         col1, col2, col3, col4 = st.columns(4)
-
         with col1:
             st.header("Total Messages")
             st.title(num_messages)
@@ -30,8 +31,35 @@ if upload_file is not None:
             st.header("Total Words")
             st.title(words)
         with col3:
-            st.header("Total Media Messages")
+            st.header("Media Shared")
             st.title(media_msg)
         with col4:
-            st.header("Total Links Shared")
+            st.header("Links Shared")
             st.title(links)
+
+        #finding most active users
+        if selected_user == 'Overall':
+            st.title("Most Active Users")
+            user_msgs_count, percentage_df = helper.most_active_users(df)
+            fig, ax = plt.subplots()
+            col1, col2 = st.columns(2)
+
+            with col1:       
+                ax = ax.bar(user_msgs_count.index, user_msgs_count.values, color = "red")
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(percentage_df)
+        
+        #wordcloud
+        st.title("WordCloud")
+        df_wc = helper.create_wordcloud(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+        #most used words
+        st.title("Most Used Words")
+        df_words = helper.most_used_words(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.barh(df_words['words'], df_words['frequency'])
+        st.pyplot(fig)
--- helper.py
+++ helper.py
@@ -1,4 +1,8 @@
 from urlextract import URLExtract
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
+
 def fetch_stats(selected_user, df):
     if selected_user != "Overall":
         df = df[df['users'] == selected_user]
@@ -16,6 +20,33 @@ def fetch_stats(selected_user, df):
     for messages in df['user_messages']:
         links.extend(extractor.find_urls(messages))
 
-
     return num_messages,len(words),media_msg,len(links)
+
+def most_active_users(df):
+    user_msgs_count = df['users'].value_counts().head(7)
+    percentage_df = round((df['users'].value_counts()/df.shape[0]*100),2).reset_index().rename(columns={'index':'users', 'count':'percent'})
+
+    return (user_msgs_count), percentage_df
  
+def create_wordcloud(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['users'] == selected_user]
+    df = df[(df['user_messages'] != '<Media omitted>\n') & (~df['user_messages'].str.contains('https', na=False))]
+
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+    df_wc = wc.generate(df['user_messages'].str.cat(sep = ""))
+
+    return df_wc
+
+def most_used_words(selected_user, df):
+    words_df = df[(df['users'] != 'chat/group notification') & (df['user_messages'] != '<Media omitted>\n')]
+    file = open("hinglish_stopwords.txt", "r", encoding="utf-8")
+    stop_words = file.read()
+    words = []
+    for messages in words_df['user_messages']:
+        for word in messages.lower().split():
+            if word not in stop_words:
+                words.append(word)    
+    df_words = pd.DataFrame(Counter(words).most_common(20), columns=['words', 'frequency'])   
+    
+    return df_words
